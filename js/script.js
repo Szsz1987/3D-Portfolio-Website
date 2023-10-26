4fb8ad650e8c5b11f31bb5d6cfce8f136c229d00
Szymon Szuliński <sszulinski@gmail.com>
2023-10-26 07:21:49 +0200

    Add function which transfer you to the profile after clicking the profile-back button

--- js/script.js
+++ js/script.js
@@ -38,8 +38,32 @@ contactMeBtn.onclick = () => {
 }
 
 //create reverse index function
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex(){
+    pageNumber--;
+    if (pageNumber < 0) {
+        pageNumber = totalPages - 1;
+    }
+}
 
 //back profile button when click
+const  backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+            
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            })
+        }, (index + 1) * 200 + 100)
+    })
+}
 
 //opening animation
 //opening animation (cover right animation)
